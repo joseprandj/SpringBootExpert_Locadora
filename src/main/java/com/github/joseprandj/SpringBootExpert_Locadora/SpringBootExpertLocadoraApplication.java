13a83ccad911b2c519fc84e84b6526e4e9589699
JJ - JoseJunior <jose.junior@doalti.com.br>
2026-05-28 10:50:58 -0300

    First commit

---
+++ src/main/java/com/github/joseprandj/SpringBootExpert_Locadora/SpringBootExpertLocadoraApplication.java
@@ -0,0 +1,13 @@
+package com.github.joseprandj.SpringBootExpert_Locadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExpertLocadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExpertLocadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/joseprandj/SpringBootExpert_Locadora/SpringBootExpertLocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joseprandj.SpringBootExpert_Locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExpertLocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
