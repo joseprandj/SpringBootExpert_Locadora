a80d26f8f7300b2a5b77407cbb06e1c23fff05ee
JJ <juniorj53@gmail.com>
2026-05-31 19:24:13 -0300

    Aula 199

--- src/test/java/com/github/joseprandj/SpringBootExpert_Locadora/service/CarServiceTest.java
+++ src/test/java/com/github/joseprandj/SpringBootExpert_Locadora/service/CarServiceTest.java
@@ -1,7 +1,7 @@
 package com.github.joseprandj.SpringBootExpert_Locadora.service;
 
 import com.github.joseprandj.SpringBootExpert_Locadora.entity.CarEntity;
-import com.github.joseprandj.SpringBootExpert_Locadora.exception.InvalidReservationException;
+import com.github.joseprandj.SpringBootExpert_Locadora.exception.EntityNotFoundException;
 import com.github.joseprandj.SpringBootExpert_Locadora.repository.CarRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -49,8 +51,40 @@ class CarServiceTest {
 
         // AsserJ
         Throwable erro = catchThrowable(() -> service.save(car));
-        assertThat(erro)
-            .isInstanceOf(IllegalArgumentException.class);
+        assertThat(erro).isInstanceOf(IllegalArgumentException.class);
+
+        Mockito.verify(repository, Mockito.never()).save(Mockito.any());
+    }
+
+    @Test
+    @DisplayName("Deve atualizar um carro")
+    void mustUpdateDataCar(){
+        Long id = 1L;
+
+        CarEntity carSaved = new CarEntity("Gol", 80, 2026);
+        Mockito.when(repository.findById(id)).thenReturn(Optional.of(carSaved));
+
+        CarEntity carUpdate = new CarEntity("Gol", 80, 2026);
+        carUpdate.setId(id);
+        Mockito.when(repository.save(Mockito.any())).thenReturn(carUpdate);
+
+        CarEntity car = new CarEntity("Sedan", 0, 2027);
+        CarEntity result = service.update(id, car);
+
+        assertEquals(result.getModel(), "Gol");
+        Mockito.verify(repository).save(Mockito.any());
+    }
+
+    @Test
+    @DisplayName("Deve retornar erro ao atualizar um carro inexistente")
+    void mustReturnErroWhenTryUpdateCarNotExists(){
+        Long id = 1L;
+        CarEntity car = new CarEntity("Sedan", 0, 2027);
+
+        Mockito.when(repository.findById(Mockito.any())).thenReturn(Optional.empty());
+
+        Throwable erro = catchThrowable(() -> service.update(id, car));
+        assertThat(erro).isInstanceOf(EntityNotFoundException.class);
 
         Mockito.verify(repository, Mockito.never()).save(Mockito.any());
     }
